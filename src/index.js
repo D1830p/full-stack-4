fda938d8f67f2203e96a3a9979b4b94051ef1fe5
Dharti <dparikh54@gmail.com>
2019-12-15 16:19:21 -0500

    Updated index.js with firebase config

--- src/index.js
+++ src/index.js
@@ -4,15 +4,17 @@ import ReactDOM from 'react-dom'
 import firebase from 'firebase'
 import 'firebase/firestore'
 import App from './App'
-import * as serviceWorker from './serviceWorker'
 
-const firebaseConfig = {}
+const firebaseConfig = {
+  apiKey: "AIzaSyBx3-4hQQXppdJI839HcT7jaepSMyRpHjk",
+  authDomain: "voting-app-da.firebaseapp.com",
+  databaseURL: "https://voting-app-da.firebaseio.com",
+  projectId: "voting-app-da",
+  storageBucket: "voting-app-da.appspot.com",
+  messagingSenderId: "751418173468",
+  appId: "1:751418173468:web:625a9e0c346f48adf997d9"
+}
 
 firebase.initializeApp(firebaseConfig)
 
 ReactDOM.render(<App />, document.getElementById('root'))
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister()
